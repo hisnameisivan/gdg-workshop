4fdb3de5cc51f6a1e422f1dc66b34553d567b98e
hisnameisivan <hisnameisivan@ya.ru>
2020-04-25 17:55:16 +0300

    cmd/workshop/main.go - add config reading

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -5,18 +5,26 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
 
+	"workshop/internal/config"
 	"workshop/internal/handler"
 )
 
 func main() {
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	h := handler.NewHandler()
 	r := chi.NewRouter()
 
 	r.Get("/hello", h.Hello)
 
 	log.Print("starting server")
-	err := http.ListenAndServe(":8080", r)
+	err = http.ListenAndServe(":8080", r)
 	log.Fatal(err)
 
 	log.Print("shutting server down")
