1440f0cb4a910b509d5d0327950e22cb5d60914e
hisnameisivan <hisnameisivan@ya.ru>
2020-04-25 14:00:20 +0300

    cmd/workshop/main.go - working server

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+
+	"workshop/internal/handler"
+)
+
 func main() {
+	h := handler.NewHandler()
+	r := chi.NewRouter()
+
+	r.Get("/hello", h.Hello)
+
+	log.Print("starting server")
+	err := http.ListenAndServe(":8080", r)
+	log.Fatal(err)
+
+	log.Print("shutting server down")
 
 }
